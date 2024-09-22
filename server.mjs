ba07e68de204b3398ea66a51464be846d2fa2364
Hafeez200 <saibhaji24@email.com>
2024-09-22 12:46:04 +0500

    make array

--- server.mjs
+++ server.mjs
@@ -1,6 +1,11 @@
-
 import express from 'express';
 import bodyParser from 'body-parser'; // Import body-parser
 
 const app = express();
 app.use(bodyParser.json()); // Use body-parser middleware
+
+// Users array
+let users = [
+  { id: 1, username: 'user1', age: 30 },
+  { id: 2, username: 'user2', age: 25 }
+];
